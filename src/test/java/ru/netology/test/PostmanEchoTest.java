83f08dd42c8dd23d7d000f665eb8f9725a12249a
Anton Sergeev <sergant3082605@mail.com>
2024-02-18 14:31:15 +0400

    initial commit

---
+++ src/test/java/ru/netology/test/PostmanEchoTest.java
@@ -0,0 +1,24 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+
+public class PostmanEchoTest {
+    @Test
+    public void shouldReturnSendData() {
+        String text = "Hi";
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body(text)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo(text))
+        ;
+    }
+}
